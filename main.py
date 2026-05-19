c3e65f4050c2224b9f6933517c59cc66df8b3096
dieuanh4127 <dieuanh.da.nb@gmail.com>
2026-05-19 21:40:08 +0700

    skibidi

---
+++ main.py
@@ -0,0 +1,60 @@
+#B1
+"""
+integer_number = 36
+float_number = 1.8
+string_text = "sigmasigmaboy"
+
+print("Số nguyên:", integer_number)
+print("Số thực:", float_number)
+print("Chuỗi:", string_text)
+"""
+"""
+#B2
+PI = 3.14
+r = 5
+
+chu_vi = 2 * PI * r
+
+print("Chu vi hình tròn là:", chu_vi)
+"""
+"""
+#B3
+a = int(input("Nhập số thứ nhất: "))
+b = int(input("Nhập số thứ hai: "))
+
+tong = a + b
+hieu = a - b
+tich = a * b
+thuong = a / b
+
+print("Tổng:", tong)
+print("Hiệu:", hieu)
+print("Tích:", tich)
+print("Thương:", thuong)
+"""
+'''
+#B4
+def sum_two_numbers(a, b):
+    return a + b
+result = sum_two_numbers(5, 7)
+
+print("Tổng là:", result)
+'''
+#B5
+name = "skibidi"
+age = 6767
+average_score = 3.6
+
+print(type(name))
+print(type(age))
+print(type(average_score))
+
+age_next_year = age + 1
+doubled_score = average_score * 2
+
+print("Tên:", name)
+print("Tuổi hiện tại:", age)
+print("Tuổi năm sau:", age_next_year)
+
+print("Điểm trung bình:", average_score)
+print("Điểm sau khi nhân đôi:", doubled_score)
